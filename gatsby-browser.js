742632dcc359d5ab92c587f6aa9080070af29872
laniakea-studio <56896609+laniakea-studio@users.noreply.github.com>
2022-02-05 10:57:41 +0200

    comment out ldfdr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,12 @@
 import React from "react";
 import LocaleContextProvider from "./src/contexts/LocaleContext";
-
+/*
 export const onRouteUpdate = ({ location }) => {
   window.ldfdr?.pageview({
     pageUrl: location.pathname,
   });
 };
-
+*/
 export const wrapPageElement = ({ element, props }) => {
   return (
     <LocaleContextProvider
