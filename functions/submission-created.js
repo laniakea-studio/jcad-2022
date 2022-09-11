97f29bd27cc8a5532755b839a479a74aad5708a5
laniakea-studio <56896609+laniakea-studio@users.noreply.github.com>
2022-09-11 19:19:37 +0300

    trying with fetch

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,19 +1,56 @@
 require("dotenv").config();
-const SibApiV3Sdk = require("sib-api-v3-sdk");
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const fetch = require("node-fetch");
+//const SibApiV3Sdk = require("sib-api-v3-sdk");
+//const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const { SENDINBLUE_API_KEY } = process.env;
 
-// Configure API key authorization: api-key
+/* Configure API key authorization: api-key
 var apiKey = defaultClient.authentications["api-key"];
 apiKey.apiKey = SENDINBLUE_API_KEY;
+*/
 
 exports.handler = async (event) => {
-  //const { email } = JSON.parse(event.body).payload;
-  const payload = JSON.parse(event.body).payload;
+  const { data } = JSON.parse(event.body).payload;
 
   //console.log(`Recieved a submission: ${email}`);
-  console.log(`Stringyfy body: ${JSON.stringify(payload)}`);
+  console.log(
+    `DATA: ${(data.email, data.webinarName, data.webinarDateAndTime)}`
+  );
 
+  return fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    url: "https://api.sendinblue.com/v3/smtp/email",
+    headers: {
+      accept: `application/json`,
+      "api-key": SENDINBLUE_API_KEY,
+      "content-type": "application/json",
+    },
+    data: {
+      to: [
+        {
+          email: "aleksi@laniakea.fi",
+          name: "Aleksi T",
+        },
+      ],
+      templateId: 1,
+      params: {
+        webinar_name: "Saneeraus- ja purkukohteiden laskenta",
+        webinar_date_and_time: "keskiviikkona 14.9. klo 9.00",
+      },
+      headers: {
+        "X-Mailin-custom":
+          "custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
+        charset: "iso-8859-1",
+      },
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`Submitted to SendinBlue: ${data}`);
+    })
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+
+  /* Integrate with Sendin Blue Packade, Problem: WEBPACK ERROR
   var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
   var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
@@ -44,6 +81,5 @@ exports.handler = async (event) => {
       console.error("Error", error);
     }
   );
-
-  console.log(`End of function`);
+  */
 };
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        parser: {
+          amd: false,
+        },
+      },
+    ],
+  },
+};
