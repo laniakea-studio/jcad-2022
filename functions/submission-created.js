bf39a1d0696a7cb8a7404611750928773793d466
laniakea-studio <56896609+laniakea-studio@users.noreply.github.com>
2022-09-11 17:54:26 +0300

    sendinblue

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,20 +1,19 @@
 require("dotenv").config();
-const fetch = require("node-fetch");
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const { SENDINBLUE_API_KEY } = process.env;
 
+// Configure API key authorization: api-key
+var apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = SENDINBLUE_API_KEY;
+
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
 exports.handler = async (event) => {
-  const payload = JSON.parse(event.body).payload;
-  console.log(`Recieved a submission: ${payload}`);
-
-  // Configure API key authorization: api-key
-  var apiKey = defaultClient.authentications["api-key"];
-  apiKey.apiKey = SENDINBLUE_API_KEY;
-
-  var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-  var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+  const { email } = JSON.parse(event.body).payload;
+  console.log(`Recieved a submission: ${email}`);
 
   sendSmtpEmail = {
     to: [
@@ -23,7 +22,7 @@ exports.handler = async (event) => {
         name: "John Doe",
       },
     ],
-    templateId: 59,
+    templateId: 1,
     params: {
       webinar_name: "Saneeraus- ja purkukohteiden laskenta",
       webinar_date_and_time: "keskiviikkona 14.9. klo 19.00",
@@ -42,4 +41,6 @@ exports.handler = async (event) => {
       console.error(error);
     }
   );
+
+  console.log(`End of function`);
 };
